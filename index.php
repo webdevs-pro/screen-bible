e35785186a080804b39253b793090a57dfbc25be
Alex Ischenko <alex.ischenko@magnificsoft.com>
2021-11-27 23:35:03 +0200

    0.1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
    <head>
       <meta charset="utf-8"/>
-      <title>Download YouTube mp3</title>
+      <title>Screen bible</title>
       <link rel="stylesheet" href="style.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <script type="text/javascript" src="script.js"></script>
@@ -25,38 +25,54 @@
          
          $bible_modules = glob(__DIR__ . "/modules/*.SQLite3");
       ?>
-      <form>
-         <select name="module" autocomplete="off">
-            <?php
-               foreach ( $bible_modules as $module_path ) {
-                  $db = new SQLite3( $module_path, SQLITE3_OPEN_READWRITE );
-                  $query = $db->query('SELECT * FROM info WHERE name == "description"');
-                  $description = $query->fetchArray()['value'];
-                  echo '<option value="' . $module_path . '">' . $description . '</option>';
-               }
-            ?>
-         </select>
-         <select name="book" size="10" autocomplete="off">
-            <?php
-               $db = new SQLite3( $bible_modules[0], SQLITE3_OPEN_READWRITE );
-               $query = $db->query('SELECT * FROM books');
-               $books = [];
-               $i = 0;
-               while ($row = $query->fetchArray()) {
-                  $i++;
-                  $books[$i]['num'] = $row['book_number'];
-                  $books[$i]['name'] = $row['long_name'];
-                  // if ($i == 66) break;
-               }
-               foreach ( $books as $book ) {
-                  echo '<option value="' . $book['num'] . '">' . $book['name'] . '</option>';
-               }
-            ?>
-         </select>
 
-
-      </form>
-
+      <div id="main">
+         <div class="main-form-wrapper">
+            <form id="main-form">
+               <select name="module" autocomplete="off">
+                  <?php
+                     foreach ( $bible_modules as $module_path ) {
+                        $db = new SQLite3( $module_path, SQLITE3_OPEN_READWRITE );
+                        $query = $db->query('SELECT * FROM info WHERE name == "description"');
+                        $description = $query->fetchArray()['value'];
+                        echo '<option value="' . $module_path . '">' . $description . '</option>';
+                     }
+                  ?>
+               </select>
+               <select name="book" size="10" autocomplete="off">
+                  <?php
+                     $db = new SQLite3( $bible_modules[0], SQLITE3_OPEN_READWRITE );
+                     $query = $db->query('SELECT * FROM books');
+                     $books = [];
+                     $i = 0;
+                     while ($row = $query->fetchArray()) {
+                        $i++;
+                        $books[$i]['num'] = $row['book_number'];
+                        $books[$i]['name'] = $row['long_name'];
+                        // if ($i == 66) break;
+                     }
+                     foreach ( $books as $book ) {
+                        echo '<option value="' . $book['num'] . '">' . $book['name'] . '</option>';
+                     }
+                  ?>
+               </select>
+               <select name="verse" size="10" autocomplete="off">
+                  <?php
+                     $db = new SQLite3( $bible_modules[0], SQLITE3_OPEN_READWRITE );
+                     $query = $db->query('SELECT text FROM verses WHERE book_number == "10" AND chapter == "1"');
+                     $verse_number = 1;
+                     while ( $verse = $query->fetchArray() ) {
+                        $book_array[$verse_number] = strip_tags(preg_replace('#(<S.*?>).*?(</S>)#', '$1$2', $verse['text']));
+                        $verse_number++;
+                     }
+                     foreach ( $book_array as $index => $verse ) {
+                        echo '<option value="' . $index . '">' . $verse . '</option>';
+                     }
+                  ?>
+               </select>
+            </form>
+         </div>
+      </div>
 
 
           <?php
@@ -73,37 +89,14 @@
             if ($i == 66) break;
          }
          
-         echo '<pre>';
-         var_export($books);
-         echo '</pre>';
+         // echo '<pre>';
+         // var_export($books);
+         // echo '</pre>';
 
          // $book_chapters_query = $db->query('SELECT DISTINCT	chapter FROM verses WHERE book_number ==' . $book_number);
          
          
       ?>
-         <div>
-            Video URL: <input id="video_url" type="text"> 
-            <input id="download" type="button" value="Download">
-         </div>
-         <div id="result"></div>
-         <script>
-            function updateText(video_url) {
-                  var ajax = new XMLHttpRequest();
-                  ajax.onreadystatechange = function() {
-                     if (this.readyState == 3) {
-                        var old_value = document.getElementById("result").innerHTML; 
-                        document.getElementById("result").innerHTML = this.responseText;
-                     }               
-                  };          
-                  var url = 'ajax.php?video_url='+video_url;
-                  ajax.open('GET', url,true);
-                  ajax.send();
-            }
-            document.getElementById("download").onclick = function(){
-               video_url = document.getElementById("video_url").value;
-                  updateText(video_url);
-            }
-         </script>
    </body>
 </html>
 
--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 jQuery(document).ready(function($) {
    
 });
+
+
+function updateText(video_url) {
+   var ajax = new XMLHttpRequest();
+   ajax.onreadystatechange = function() {
+      if (this.readyState == 3) {
+         var old_value = document.getElementById("result").innerHTML; 
+         document.getElementById("result").innerHTML = this.responseText;
+      }               
+   };          
+   var url = 'ajax.php?video_url='+video_url;
+   ajax.open('GET', url,true);
+   ajax.send();
+}
+document.getElementById("download").onclick = function(){
+video_url = document.getElementById("video_url").value;
+   updateText(video_url);
+}
